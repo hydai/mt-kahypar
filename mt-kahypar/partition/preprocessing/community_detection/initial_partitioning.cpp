07cd4e1bad19e3d1c5b365dc6f62ef62410f3f40
Simon <ukhik@student.kit.edu>
2021-04-18 20:14:23 +0200

    running the whole partitioner in preprocessing not only the initial partitioner

--- mt-kahypar/partition/preprocessing/community_detection/initial_partitioning.cpp
+++ mt-kahypar/partition/preprocessing/community_detection/initial_partitioning.cpp
@@ -1,17 +1,19 @@
 #include "initial_partitioning.h"
 #include <mt-kahypar/partition/initial_partitioning/i_initial_partitioner.h>
+#include "mt-kahypar/partition/multilevel.h"
 #include "mt-kahypar/partition/factories.h"
 
 namespace mt_kahypar::community_detection {
   ds::Clustering run_initial_partioning(Hypergraph & hypergraph, const Context& context, bool disable_randomization){
     ds::Clustering communities(hypergraph.initialNumNodes());
-    PartitionedHypergraph phg(2, hypergraph);
 
-    std::unique_ptr<IInitialPartitioner> initial_partitioner =
-      InitialPartitionerFactory::getInstance().createObject(
-        context.initial_partitioning.mode, phg,
-        context, true, TBBNumaArena::GLOBAL_TASK_GROUP);
-    initial_partitioner->initialPartition();
+    Context community_detection_context(context);
+    community_detection_context.refinement.label_propagation.algorithm = LabelPropagationAlgorithm::do_nothing;
+    community_detection_context.refinement.fm.algorithm = FMAlgorithm::do_nothing;
+    community_detection_context.partition.verbose_output = false;
+    PartitionedHypergraph phg = multilevel::partition(
+      hypergraph, community_detection_context, false, TBBNumaArena::GLOBAL_TASK_GROUP);
+
 
     tbb::parallel_for(ID(0), hypergraph.initialNumNodes(), [&](const HypernodeID hn) {
       communities[hn] = phg.partID(hn);
